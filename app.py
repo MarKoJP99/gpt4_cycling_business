6e09aa10c42c3f73bc6c64b121a50bedc1ff4320
MarKo <marko@MarKonoMacBook-Pro.local>
2023-04-29 14:07:56 +0900

    product categories base working

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
-# Main application file that sets up the Flask app and routes
-
 from flask import Flask, render_template, request, url_for, redirect
 from db import search_products, init_db
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
+    return render_template("home.html")
+
+@app.route("/wheels")
+def wheels():
+    return render_template("wheels.html")
+
+@app.route("/frame_sets")
+def frame_sets():
+    return render_template("frame_sets.html")
+
+@app.route("/components")
+def components():
+    return render_template("components.html")
+
+@app.route("/results/<product_type>", methods=["GET", "POST"])
+def results(product_type):
     if request.method == "POST":
         search_query = request.form["search"]
-        results = search_products(search_query)
-        if results:
-            return render_template("results.html", results=results)
-        else:
-            return render_template("no_results.html", search_query=search_query)
-    return render_template("home.html")
+        results = search_products(product_type, search_query)  # Update your search_products function to handle product_type
+        return render_template(f"results_{product_type}.html", results=results)
+    return redirect(url_for("home"))
 
 @app.route("/back")
 def back():
     return redirect(url_for("home"))
-
 
 if __name__ == "__main__":
     init_db(app)
